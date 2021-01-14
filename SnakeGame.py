e2c9fe7e2ad2bb6214b7daa85ec783e9bfa15aa8
ShrutiRawal <shruti456rawal@gmail.com>
2021-01-14 15:04:38 +0530

    Score logic built

--- SnakeGame.py
+++ SnakeGame.py
@@ -128,6 +128,7 @@ class Main:
     def draw_elements(self):
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
@@ -140,6 +141,21 @@ class Main:
 
     def game_over(self):
         self.snake.reset()
+
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text, True, (56, 74, 12))
+        score_x = int(cell_size * cell_no - 60)
+        score_y = int(cell_size * cell_no - 40)
+        score_rect = score_surface.get_rect(center=(score_x, score_y))
+        apple_rect = apple.get_rect(midright=(score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 6,
+                              apple_rect.height)
+
+        pygame.draw.rect(screen, (167, 209, 61), bg_rect)
+        screen.blit(score_surface, score_rect)
+        screen.blit(apple, apple_rect)
+        pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
 
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_no or not 0 <= self.snake.body[0].y < cell_no:
@@ -156,6 +172,7 @@ cell_no = 20
 screen = pygame.display.set_mode((cell_size*cell_no, cell_size*cell_no))
 clock = pygame.time.Clock()
 apple = pygame.image.load('apple.png').convert_alpha()
+game_font = pygame.font.Font('PoetsenOne-Regular.ttf', 25)
 
 main_game = Main()
 
